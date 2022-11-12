af5e3f225ef551919a97d8ec65190a5361f7b5db
Svetlana <iilsojar@gmail.com>
2022-11-12 15:04:15 +0100

    Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -3,32 +3,23 @@
 //1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 //6, 1, 33 -> [6, 1, 33]
 
-//int [] array = 
+int [] array = new int [8];
+FillArray(array);
+PrintArray(array);
 
-//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-//452 -> 11
-//82 -> 10
-//9012 -> 12
-
-Console.Clear();
-Console.Write("введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int sumOfNumbers = SumOfNumbers(number);
-Console.WriteLine($"сумма цифр в числе {number} -> {sumOfNumbers}");
-
-int SumOfNumbers (int num)
+void FillArray (int [] arr)
 {
-        int sum = 0;
-        int num_last = default;
-        while (num > 0)
-            {
-                num_last = num % 10;
-                num = num / 10;
-                sum = sum + num_last;
-                Console.Write($"{num_last} ");
-            }   
-        return sum;
+    int length = arr.Length;
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = 1;
     }
-
-
-
+}
+void PrintArray (int [] arr)
+{
+    int length = arr.Length;
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"{i} ");
+    }
+}
